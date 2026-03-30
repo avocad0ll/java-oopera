0a0802902afc24676a422a33e8d20e9788967576
jorikgrozniy <jorikgrsskom218@gmail.com>
2026-03-30 21:16:12 +0300

    fixed

--- src/Show.java
+++ src/Show.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
 public class Show {
-    String title;
-    int duration;
-    Director director;
-    ArrayList<Actor> listOfActors;
+    protected String title;
+    private int duration;
+    private Director director;
+    private ArrayList<Actor> listOfActors;
 
     public Show(String title, int duration, Director director) {
         this.title = title;
